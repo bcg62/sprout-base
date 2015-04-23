a933af93d62405bbf73a1929eb6d7ab62c1bbd71
Brendan Germain <Brendan.Germain@nasdaqomx.com>
2015-04-23 10:37:47 -0400

    only enforce the library on osx

--- libraries/user.rb
+++ libraries/user.rb
@@ -1,2 +1,5 @@
-raise "sudo to root before running" if ENV['SUDO_USER'].nil?
-raise "should not be root" if ENV['SUDO_USER'].strip == "root"
+case node['platform_family']
+when 'mac_os_x'
+  raise "sudo to root before running" if ENV['SUDO_USER'].nil?
+  raise "should not be root" if ENV['SUDO_USER'].strip == "root"
+end
